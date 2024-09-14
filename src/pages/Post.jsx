de29fc418e2fe415acf2e6d18bd99f25742eb3ce
Sameer9823 <sameerselokar924@gmail.com>
2024-09-14 17:44:24 +0530

    footerupdate

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -58,7 +58,7 @@ export default function Post() {
                 <div className="w-full mb-6">
                     <h1 className="text-2xl font-bold">{post.title}</h1>
                 </div>
-                <div className="browser-css px-[5rem]">
+                <div className="browser-css lg:px-[5rem] px-4">
                     {parse(post.content)}
                     </div>
             </Container>
